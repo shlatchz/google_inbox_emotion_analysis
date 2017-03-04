dae662dd6e89c036112e9155e0e0b461529b7543
shlatchz <shlatchz@gmail.com>
2017-03-04 16:46:11 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,6 @@ var rxLookfor = /^https\:\/\/inbox\.google\.com\/\?pli=1$/;
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     if (changeInfo.status === "complete" && 
 	    rxLookfor.test(tab.url)) {
-		console.log({ action: "onUpdated" , tabId: tabId, url: tab.url });
         chrome.tabs.sendMessage(tabId, { type: 'url-update', tabId: tabId });
     }
 });
@@ -13,7 +12,6 @@ chrome.tabs.onActivated.addListener(function(changeInfo) {
 	var tab = chrome.tabs.get(changeInfo.tabId, function(tab) {
 		var tabId = changeInfo.tabId;
         if (rxLookfor.test(tab.url)) {
-			console.log({ action: "onActivated" , tabId: tabId, url: tab.url });
 			chrome.tabs.sendMessage(tabId, { type: 'url-update', tabId: tabId });
 		}
     });
